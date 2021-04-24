2d2ddd678f46f48654784fada83254327f9c2b3d
Igor Yakubov <igor.yakubov@telus.com>
2021-04-23 21:59:47 -0700

    fix contract deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Casino = artifacts.require("./Casino.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(web3.toWei(0.1, 'ether'), {gas: 3000000});
+  deployer.deploy(Casino, 10000000, 50);
 };
