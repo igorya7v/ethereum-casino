e508a25c6afd19020f4f72ce6d4e4a2d7b421418
Igor Yakubov <igor.yakubov@telus.com>
2021-04-24 19:55:13 -0700

    reduce eth confirmations and timeout blokcs

--- truffle-config.js
+++ truffle-config.js
@@ -66,8 +66,8 @@ module.exports = {
      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
      network_id: 3,       // Ropsten's id
      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+     confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+     timeoutBlocks: 50,  // # of blocks before a deployment times out  (minimum/default: 50)
      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      }
     // Useful for private networks
